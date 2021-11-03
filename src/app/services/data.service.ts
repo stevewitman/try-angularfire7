654c397b33de7ed04fab6a72335330dac599d7a1
Steve Witman <stevewitman@gmail.com>
2021-11-02 23:09:35 -0600

    add remaining crud methods to data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,18 +1,49 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collectionData } from '@angular/fire/firestore';
-import { collection } from '@firebase/firestore';
+import { Firestore, collectionData, docData } from '@angular/fire/firestore';
+import { addDoc, collection, deleteDoc, doc, DocumentData, DocumentReference } from '@firebase/firestore';
+import { Observable } from 'rxjs';
+
+export interface Post {
+  id?: string;
+  title: string;
+  description: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor(private firestore: Firestore) {}
 
-  constructor(private firestore: Firestore) { }
-
-  getPosts() {
+  getPosts(): Observable<Post[]> {
     const postsRef = collection(this.firestore, 'posts');
     return collectionData(postsRef, {
-      idField: 'id'
-    });
-  }  
+      idField: 'id',
+    }) as Observable<Post[]>;
+  }
+
+  getPostById(id: string): Observable<Post> {
+    const postDocRef = doc(this.firestore, `notes: ${id}`);
+    return docData(postDocRef, {
+      idField: 'id',
+    }) as Observable<Post>;
+  }
+
+  addPost(post: Post) {
+    const postsRef = collection(this.firestore, 'notes');
+    return addDoc(postsRef, post);
+  }
+
+  updatePost(post: Post) {
+    const postDocRef = doc(this.firestore, `posts/${post.id}`);
+    return this.updateDoc(postDocRef, { title: post.title, description: post.description });
+  }
+  updateDoc(postDocRef: DocumentReference<DocumentData>, arg1: { title: string; description: string; }) {
+    throw new Error('Method not implemented.');
+  }
+
+  deletePost(post: Post) {
+    const postDocRef = doc(this.firestore, `posts/${post.id}`);
+    return deleteDoc(postDocRef);
+  }
 }
